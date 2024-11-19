f3888f0201b2d39b541e811a6d822691bab83781
shakil <ahmadshakil22255@gmail.com>
2024-11-19 22:30:22 +0600

    koita message jabe sheta bole deoa jabe etao add korlam

--- autoPastText.py
+++ autoPastText.py
@@ -1,7 +1,24 @@
+# eta message jetei thakbe:-
+
+# import pyautogui
+# import time
+# while True:
+#     time.sleep(4)
+#     pyautogui.typewrite('i love you.')
+#     time.sleep(2)
+#     pyautogui.press('enter')
+
+
+
+
+# koita message jabe sheta bole deoa jabe:-
+
 import pyautogui
 import time
-while True:
+message = 10
+while message > 0:
     time.sleep(4)
-    pyautogui.typewrite('i love you.')
+    pyautogui.typewrite('problem ki')
     time.sleep(2)
     pyautogui.press('enter')
+    message = message - 1
